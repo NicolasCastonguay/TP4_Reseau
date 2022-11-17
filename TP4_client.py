11a3e7945ee82b369192c3a728866b1c6f859dc6
Nicolas Castonguay <nicolas.castonguay.3@ulaval.ca>
2022-11-17 00:42:16 -0500

    TP4_debut_client

--- TP4_client.py
+++ TP4_client.py
@@ -1,7 +1,7 @@
 """\
 GLO-2000 Travail pratique 4 - Client
 Noms et numéros étudiants:
--
+- Nicolas Castonguay 536 774 848
 -
 -
 """
@@ -11,7 +11,7 @@ import getpass
 import json
 import socket
 import sys
-
+import getpass
 import glosocket
 import gloutils
 
@@ -26,6 +26,9 @@ class Client:
         Prépare un attribut `_username` pour stocker le nom d'utilisateur
         courant. Laissé vide quand l'utilisateur n'est pas connecté.
         """
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._socket.connect(destination, gloutils.APP_PORT)
+        self._username = ""
 
     def _register(self) -> None:
         """
@@ -35,6 +38,16 @@ class Client:
         Si la création du compte s'est effectuée avec succès, l'attribut
         `_username` est mis à jour, sinon l'erreur est affichée.
         """
+        input_username = input("Entrez votre nom d'utilisateur : ")
+        self._password = getpass.getpass("Entrez votre mot de passe : ")
+        auth_payload = gloutils.AuthPayload(input_username, self._password)
+        glosocket.send_msg(self._socket, gloutils.GloMessage(
+            gloutils.Headers.AUTH_REGISTER, auth_payload))
+        msg_received = json.load(glosocket.recv_msg(self._socket))
+        if msg_received["header"] == gloutils.Headers.OK:
+            self._username = input_username
+        else:
+            print(msg_received["payload"])
 
     def _login(self) -> None:
         """
@@ -44,12 +57,24 @@ class Client:
         Si la connexion est effectuée avec succès, l'attribut `_username`
         est mis à jour, sinon l'erreur est affichée.
         """
+        input_username = input("Entrez votre nom d'utilisateur : ")
+        self._password = getpass.getpass("Entrez votre mot de passe : ")
+        auth_payload = gloutils.AuthPayload(input_username, self._password)
+        glosocket.send_msg(self._socket, gloutils.GloMessage(
+            gloutils.Headers.AUTH_LOGIN, auth_payload))
+        msg_received = json.load(glosocket.recv_msg(self._socket))
+        if msg_received["header"] == gloutils.Headers.OK:
+            self._username = input_username
+        else:
+            print(msg_received["payload"])
 
     def _quit(self) -> None:
         """
         Préviens le serveur de la déconnexion avec l'entête `BYE` et ferme le
         socket du client.
         """
+        glosocket.send_msg(
+            self._socket, gloutils.GloMessage(gloutils.Headers.BYE))
 
     def _read_email(self) -> None:
         """
@@ -64,6 +89,21 @@ class Client:
         S'il n'y a pas de courriel à lire, l'utilisateur est averti avant de
         retourner au menu principal.
         """
+        glosocket.send_msg(self._socket, gloutils.GloMessage(
+            gloutils.Headers.INBOX_READING_REQUEST))
+        msg_received = json.loads(glosocket.recv_msg(self._socket))
+        email_list = msg_received["payload"]
+        if email_list.lenght() == 0:
+            sys.exit(1)
+        for email in email_list:
+            print(email)
+        choice = input("Entrez le numéro du courriel désiré : ")
+        glosocket.send_msg(self._socket, gloutils.GloMessage(
+            gloutils.Headers.INBOX_READING_CHOICE, choice))
+        email_received = json.loads(
+            glosocket.recv_msg(self._socket))["payload"]
+        email_info = json.loads(email_received)
+        print(gloutils.EMAIL_DISPLAY.format(email_info))
 
     def _send_email(self) -> None:
         """
@@ -76,6 +116,17 @@ class Client:
 
         Transmet ces informations avec l'entête `EMAIL_SENDING`.
         """
+        dest = input("Entrez l'email du destinataire : ")
+        subject = input("Entrez le sujet du email : ")
+        print("Entrez le contenu de votre courriel : ")
+        body = ""
+        while True:
+            line = input()
+            if line == ".":
+                break
+            body += line + "\n"
+        glosocket.send_msg(self._socket, json.dumps(gloutils.GloMessage(header=gloutils.Headers.EMAIL_SENDING,
+                                                                        payload=gloutils.EmailContentPayload(date=gloutils.get_current_utc_time(), subject=subject, content=body, destination=dest, sender=self._username))))
 
     def _check_stats(self) -> None:
         """
